f50aa3764047e3aa0bea87506855a7590652aa8a
Louis Fourie <lfourie@Louiss-MBP.modem>
2022-08-21 14:46:58 +0800

    remove groups

--- _main.tf
+++ _main.tf
@@ -83,38 +83,3 @@ resource "zpa_application_segment" "dev5_application" {
     id = [zpa_server_group.dev_servers.id]
   }
 }
-
-// *************************** cut ***********************
-// Create Server Group
-resource "zpa_server_group" "dev_servers" {
-  name              = "dev Servers"
-  description       = "dev Servers created from terraform"
-  enabled           = true
-  dynamic_discovery = true
-  app_connector_groups {
-    id = [data.zpa_app_connector_group.dc_connector_group.id]
-  }
-  //servers {
-  //  id = [zpa_application_server.dev_app_server.id]
-  }
-
-// Create application Server
-//resource "zpa_application_server" "dev_app_server" {
-//  name        = "dev app Server"
-//  description = "dev app Server"
-//  address     = "dev.clydetwo.guru"
-//  enabled     = true
-// }
-
-// Create Segment Group
-resource "zpa_segment_group" "dev_app_group" {
-  name            = "dev app SG"
-  description     = "id"
-  enabled         = true
-  policy_migrated = true
-}
-
-// Retrieve app Connector Group
-data "zpa_app_connector_group" "dc_connector_group" {
-  name = "clydetwo_skytap_ac1"
-}
